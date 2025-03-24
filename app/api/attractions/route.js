5c326834b4cf5ca0afe1977d831e18c025a16ec1
Pattrapa Choeisiri <Bunpatt2@gmail.com>
2025-03-24 14:30:47 +0700

    addapiupdate attractions

--- app/api/attractions/route.js
+++ app/api/attractions/route.js
@@ -14,15 +14,33 @@ export async function GET(request) {
   }
 }
 
+export async function POST(request){
+  try {
+      const {name, detail,coverimage, latitude, longitude} = await request.json();
+      const [result] = await db.query(
+          'INSERT INTO attractions (name, detail,coverimage, latitude, longitude) VALUES (?,?,?,?,?)',[name, detail,coverimage, latitude, longitude]
+      );
+      return NextResponse.json({id:result.insertId,name, detail,coverimage, latitude, longitude}, {status: 200});
+  } catch (error){
+    return NextResponse.json({error:error}, {status: 500})
+  }
+}
+
 export async function PUT(request) {
   try {
-    const { name, detail, coverimage, latitude, longitude } = await request.json()
-    const [result] = await db.query('INSERT INTO attractions (name, detail,coverimage, latitude, longitude) VALUES (?,?,?,?,?)', [name, detail, coverimage, latitude, longitude]
-    );
-    return NextResponse.json({ id: result.insertId, name, detail, coverimage, latitude, longitude }, { status: 200 });
+    const { id, name, detail, coverimage, latitude, longitude } = await request.json()
+    const [result] = await db.query(
+      'UPDATE attractions SET name=?, detail=?, coverimage=?, latitude=?, longitude=? WHERE id = ?',
+      [name, detail, coverimage, latitude, longitude,id]);
+    if (result.affectedRows == 0) {
+      return NextResponse.json({ error: "ID Not found" }, { status: 404 });
+    }
+
+    return NextResponse.json({ message: "Updated", id }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: error }, { status: 500 })
   }
 }
+
 
 
